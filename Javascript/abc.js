db930ce9e2cd48a04af1ed402af46d51a674ed05
Jonathan Dabre <dabrejonathan.manuel@wissen.com>
2025-02-27 12:28:10 +0530

    Add JavaScript file demonstrating variable types and their datatypes

--- Javascript/abc.js
+++ Javascript/abc.js
@@ -1,3 +1,35 @@
 console.log("Hello everybody")
 a = 12;
 console.log(a)
+
+var a;
+console.log("a value : "+a);
+console.log("a datatype : "+typeof(a));
+a = 10;
+console.log("a value : "+a);
+console.log("a datatype : "+typeof(a));
+a = false;
+console.log("a value : "+a);
+console.log("a datatype : "+typeof(a));
+a = "hello";
+console.log("a value : "+a);
+console.log("a datatype : "+typeof(a));
+a = 'c';
+console.log("a value : "+a);
+console.log("a datatype : "+typeof(a));
+a = 3.1;
+console.log("a value : "+a);
+console.log("a datatype : "+typeof(a));
+
+a = new Date();
+console.log("a value : "+a);
+console.log("a datatype : "+typeof(a));
+
+a = null;
+console.log("a value : "+a);
+console.log("a datatype : "+typeof(a));
+
+a = ()=>{};
+console.log("a value : "+a);
+console.log("a datatype : "+typeof(a));
+
